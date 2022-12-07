02786548784b51c8e5f768ca4128620e1ecc2ef1
tjanitsc <37362339+tjanitsc@users.noreply.github.com>
2022-12-07 15:35:26 +0100

    Update storage_policy.tf

--- ucsc-policies/storage_policy.tf
+++ ucsc-policies/storage_policy.tf
@@ -13,21 +13,9 @@ resource "intersight_storage_storage_policy" "storage_policy" {
     key   = var.tags.key2
     value = var.tags.value2
   }
-  retain_policy_virtual_drives = var.storage_retain_policy_virtual_drives # bool, true
+
   unused_disks_state           = var.storage_unused_disks_state           # "UnconfiguredGood"
-  virtual_drives {
-    name                = var.storage_vd_name # "RAID0_1"
-    object_type         = "storage.VirtualDriveConfig"
-    boot_drive          = var.storage_vd_boot_drive          # bool, true
-    drive_cache         = var.storage_vd_drive_cache         # "NoChange"
-    expand_to_available = var.storage_vd_expand_to_available # false
-    io_policy           = var.storage_vd_io_policy           # "Direct"
-    access_policy       = var.storage_vd_access_policy       # "ReadWrite"
-    read_policy         = var.storage_vd_read_policy         # "NoReadAhead"
-    size                = var.storage_vd_size                # 285148
-    write_policy        = var.storage_vd_write_policy        # "WriteThrough"
-    disk_group_policy   = intersight_storage_disk_group_policy.disk_group_policy.moid
-  }
+  
   profiles {
     moid        = intersight_server_profile.server_profile.moid
     object_type = "server.Profile"
