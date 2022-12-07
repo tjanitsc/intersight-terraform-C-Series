40091acad88947dbe3a55fde63b621badf5714a2
tjanitsc <37362339+tjanitsc@users.noreply.github.com>
2022-12-07 15:32:47 +0100

    Update disk_group_policy.tf

--- ucsc-policies/disk_group_policy.tf
+++ ucsc-policies/disk_group_policy.tf
@@ -1,9 +1,5 @@
 resource "intersight_storage_drive_group" "drive_group_policy" {
   name        = "${var.prefix}_disk_group_policy"
-  organization {
-    object_type = "organization.Organization"
-    moid        = data.intersight_organization_organization.org_data.results[0].moid
-  }
   tags {
     key   = var.tags.key1
     value = var.tags.value1
